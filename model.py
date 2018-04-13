e398831671d7140dbabc49195e8a3a2232248589
neilzhu2 <neilzhu@umich.edu>
2018-04-12 23:22:22 -0400

    1-4

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
 def init(app):
     global entries
@@ -21,11 +22,36 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
     now = datetime.now()
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+
+    if 'id' not in entries[0]:
+        next_id = 0
+        leng = len(entries) - 1
+        for e in entries:
+            if 'id' not in e:
+                e['id'] = str(leng - next_id)
+            next_id += 1
+    else:
+        next_id = int(entries[0]['id']) + 1
+
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": str(next_id)}
     entries.insert(0, entry) ## add to front of list
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not write entries to file.")
+
+def delete_entry(id):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    for e in entries:
+        if e['id'] == id:
+            entries.remove(e)
+            break
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
